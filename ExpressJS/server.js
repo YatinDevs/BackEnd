aaa58728eb5ef70a1a9ec28c760b6eff456b7d17
YatinDevs <c.yatin727@gmail.com>
2023-12-26 20:24:55 +0530

    response

--- ExpressJS/server.js
+++ ExpressJS/server.js
@@ -2,4 +2,20 @@ const fs = require("fs");
 const express = require("express"); // npm i express
 // create server with express
 const server = express();
-server.listen(8080);
+
+// Data fetch
+const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
+const products = data.products;
+
+// code here
+server.get("/", (req, res) => {
+  console.log("home");
+  res.json(products);
+  //   res.send("<h1>Welcome</h1>");
+  //   res.sendFile("E:/BackEnd/ExpressJS/index.html");
+});
+
+// as convention listener should be at end
+server.listen(8080, () => {
+  console.log("server started");
+});
