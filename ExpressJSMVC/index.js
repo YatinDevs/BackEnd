6ecf4a71bdbadeee2968e6bcce5f49084bda84fc
YatinDevs <c.yatin727@gmail.com>
2023-12-29 12:54:20 +0530

    RESTAPI

--- ExpressJSMVC/index.js
+++ ExpressJSMVC/index.js
@@ -36,6 +36,31 @@ const getProduct = (req, res) => {
   res.status(200).json(product);
 };
 
+const replaceProduct = (req, res) => {
+  const id = parseInt(req.params.id);
+  const productidx = products.findIndex((prd) => prd.id === id);
+  products.splice(productidx, 1, { ...req.body, id: id });
+  res.status(201).json();
+};
+
+const updateProduct = (req, res) => {
+  const id = parseInt(req.params.id);
+  const prdIdx = products.findIndex((prd) => prd.id === id);
+  const product = products[prdIdx];
+  products.splice(prdIdx, 1, { ...product, ...req.body });
+  res.status(201).json();
+};
+const deleteProduct = (req, res) => {
+  const id = parseInt(req.params.id);
+  const prdIdx = products.findIndex((prd) => prd.id === id);
+  const productpopped = products[prdIdx];
+  products.splice(prdIdx, 1);
+  res.status(200).json(productpopped);
+};
+
+server.get("/", (req, res) => {
+  res.send("<h1>Hello</h1>");
+});
 // Rest API's -> CRUD Operations
 
 // CREATE POST /products --> post by req.body
@@ -46,6 +71,15 @@ server.get("/products", getAllProducts);
 
 // READ GET /products/:id -> get by req.params
 server.get("/products/:id", getProduct);
+
+// UPDATE PUT /products/:id -> get by req.params
+server.put("/products/:id", replaceProduct);
+
+// UPDATE patch /products/:id -> get by req.params
+server.patch("/products/:id", updateProduct);
+
+// DELETE delete /products/:id -> get by req.params
+server.delete("/products/:id", deleteProduct);
 
 // server listener
 server.listen(8080, () => {
